c8d493f18f40bdf772cb5e57fe4900fcb3ecaca0
ShawnRtc <83451787+ShawnRtc@users.noreply.github.com>
2021-05-20 11:57:00 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,63 @@
+let userName=document.getElementById("txtUserName");
+let email=document.getElementById("txtEmail");
+let pwd=document.getElementById("txtNum");
+let conPwd=document.getElementById("txtEnquiry");
+let form=document.querySelector("form");
+
+function validateInput(){
+    //check username is empty 
+    if(userName.value.trim()===""){
+       onError(userName,"User Name cannot be empty");
+    }else{
+        onSuccess(userName);
+    }
+    if(email.value.trim()===""){
+        onError(email,"Email cannot be empty");
+    }else{
+        if(!isValidEmail(email.value.trim())){
+            onError(email,"Email is not valid");
+        }else{
+            onSuccess(email);
+        }
+    }
+
+    //password
+    if(number.value.trim()===""){
+        onError(pwd,"Number cannot be empty");
+     }else{
+         onSuccess(number);
+     }
+     if(enquiry.value.trim()===""){
+        onError(enquiry,"Enquiry cannot be empty");
+     }else
+         onSuccess(enquiry);
+     }
+
+
+document.querySelector("button")
+.addEventListener("click",(event)=>{
+    event.preventDefault();
+    validateInput();
+});
+
+function onSuccess(input){
+    let parent=input.parentElement;
+    let messageEle=parent.querySelector("small");
+    messageEle.style.visibility="hidden"; 
+    parent.classList.remove("error");
+    parent.classList.add("success");  
+}
+function onError(input,message){
+    let parent=input.parentElement;
+    let messageEle=parent.querySelector("small");
+    messageEle.style.visibility="visible";
+    messageEle.innerText=message;  
+    parent.classList.add("error");
+    parent.classList.remove("success");
+
+}
+
+function isValidEmail(email){
+   return /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(email);
+}
+
